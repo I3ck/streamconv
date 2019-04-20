cae7cbff43371f9e70ee02b66ce0d2796e90edd8
Martin Buck <buckmartin@buckmartin.de>
2019-04-20 18:02:52 +0200

    drop of now unused megaparsec

--- src/Parsers.hs
+++ src/Parsers.hs
@@ -3,38 +3,13 @@
 module Parsers where
 
 import Types
-import Data.Void
 import Data.Text
-import qualified Data.Text.Lazy as L
 import qualified Data.Attoparsec.Text.Lazy as A
-import Text.Megaparsec
-import Text.Megaparsec.Char
-import qualified Text.Megaparsec.Char.Lexer as LE
-
 
 --------------------------------------------------------------------------------
 
-type Parser = Parsec Void L.Text
-
---------------------------------------------------------------------------------
-
---- TODO not parsing lazyly
-xyz :: L.Text -> L.Text -> Parser [Position]
-xyz delimval delimline = many $ xyzLine delimval delimline ---TODO use delimline only in this parser and dont require it at the end
-
-xyzLine :: L.Text -> L.Text -> Parser Position
+xyzLine :: Text -> Text -> A.Parser Position
 xyzLine delimval delimline = do
-  x <- LE.float
-  string delimval
-  y <- LE.float
-  string delimval
-  z <- LE.float
-  string delimline
-
-  pure $ Position x y z
-
-xyzLine' :: Text -> Text -> A.Parser Position
-xyzLine' delimval delimline = do
   x <- A.double
   A.string delimval
   y <- A.double
@@ -43,19 +18,3 @@ xyzLine' delimval delimline = do
   A.string delimline
 
   pure $ Position x y z
-
-
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
-
---double :: Parser Double
---double = try (L.signed sc L.float) <|> (fromIntegral <$> L.signed sc L.decimal)
-
---------------------------------------------------------------------------------
-
-sc :: Parser ()
-sc = LE.space space1 lineComment blockComment
-  where
-    lineComment  = LE.skipLineComment "//"
-    blockComment = LE.skipBlockComment "/*" "*/"
--- src/Producers.hs
+++ src/Producers.hs
@@ -5,7 +5,6 @@ import Conduit
 import Data.Text
 import qualified Data.Text.Lazy.IO as L
 import qualified Parsers as P
---import Text.Megaparsec
 import Data.Attoparsec.Text.Lazy as A
 
 --------------------------------------------------------------------------------
@@ -16,24 +15,9 @@ xyz path delimval delimline = do
     go blob
   where
     go input = do
-      let result = A.parse (P.xyzLine' delimval delimline) input
+      let result = A.parse (P.xyzLine delimval delimline) input
       case result of
         A.Fail _ _ _ -> pure ()
         A.Done rest x -> do
           yield x
           go rest
-{-
-xyz :: String -> Text -> Text -> ConduitT () Position IO ()
-xyz path delimval delimline = do
-    blob   <- liftIO $ L.readFile path
-    let initial = State blob 0 (PosState blob 0 (SourcePos "" (mkPos 0) (mkPos 0)) (mkPos 0) "") ---TODO PosState constructor likely incorrect
-    go initial
-  where
-    go input = do
-      let (newinput, parsed) = runParser' (P.xyzLine delimval delimline) input
-      case parsed of
-        Left _  -> pure () --TODO consider error handling
-        Right x -> do
-          yield x
-          go newinput
--}
