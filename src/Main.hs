ac221f817e13eab9cd55835e107e897266cb1a85
Martin Buck <buckmartin@buckmartin.de>
2019-04-20 16:27:44 +0200

    usage of buffers for better performance

--- src/Main.hs
+++ src/Main.hs
@@ -20,9 +20,9 @@ main = do --withSourceFile "input.tmp" $ \source ->
        --withSinkFile "output.tmp" $ \sink -> do
        --runConduit $ source .| decodeUtf8C .| test .| encodeUtf8C .| sink
        runConduit $ fileChars "input.tmp" .| test2 .| sinkList
-       runConduit $ (yieldMany . replicate 10000000 $ Position 3 4 5) .| mapC (xyzToStr ";" "\n") .| stringSink "output1.tmp"
-       runConduit $ (yieldMany . replicate 1000 $ Position 3 4 5) .| mapC (xyToStr  ";" "\n") .| stringSink "output2.tmp"
-       runConduit $ xyz "output1.tmp" ";" "\n" .| mapC (xyToStr  ";" "\n") .| stringSink "output3.tmp"
+       runConduit $ (yieldMany . replicate 10000000 $ Position 3 4 5) .| xyzToStrC ";" "\n" .| stringSink "output1.tmp"
+       runConduit $ (yieldMany . replicate 1000 $ Position 3 4 5) .| xyToStrC  ";" "\n" .| stringSink "output2.tmp"
+       runConduit $ xyz "output1.tmp" ";" "\n" .| xyToStrC  ";" "\n" .| stringSink "output3.tmp"
        pure ()
 
 --------------------------------------------------------------------------------
@@ -69,6 +69,43 @@ xyzToStr delimval delimline v =
   . ((show . getz $ v) ++)
   . (delimline ++))
   ""
+
+xyToStrC :: (Monad m, X a, Y a) => String -> String -> ConduitT a String m ()
+xyToStrC delimval delimline = go []
+  where
+    bufferSize = 100 --TODO param
+    go buffer = do
+      may <- await
+      case may of
+        Nothing -> yield $ concatMap (xyToStr delimval delimline) buffer
+        Just v  -> if length buffer > bufferSize
+                   then do 
+                     yield $ concatMap (xyToStr delimval delimline) (v : buffer)
+                     go []
+                   else go (v : buffer)
+
+xyzToStrC :: (Monad m, X a, Y a, Z a) => String -> String -> ConduitT a String m ()
+xyzToStrC delimval delimline = go []
+  where
+    bufferSize = 100 --TODO param
+    go buffer = do
+      may <- await
+      case may of
+        Nothing -> yield $ concatMap (xyzToStr delimval delimline) buffer
+        Just v  -> if length buffer > bufferSize
+                   then do 
+                     yield $ concatMap (xyzToStr delimval delimline) (v : buffer)
+                     go []
+                   else go (v : buffer)
+  {-
+    (((show . getx $ v) ++)
+  . (delimval ++)
+  . ((show . gety $ v) ++)
+  . (delimval ++)
+  . ((show . getz $ v) ++)
+  . (delimline ++))
+  ""
+  -}
 
 ---TODO rename since writes to file
 ---TODO consider usage Text everywhere
