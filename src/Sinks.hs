964cfefe910a1bc3501f1a8c2d9195b541ca27c5
Martin Buck <buckmartin@buckmartin.de>
2019-04-22 12:42:09 +0200

    now writing correct point count to ply file

--- src/Sinks.hs
+++ src/Sinks.hs
@@ -31,21 +31,29 @@ plyAsciiSink path = do
   h <- liftIO $ openFile path WriteMode --TODO consider withFile
   liftIO $ hPutStrLn h "ply"
   liftIO $ hPutStrLn h "format ascii 1.0"
-  liftIO $ hPutStrLn h "element vertex 0" ---TODO must update this count later on
+  placeholderPos <- liftIO $ hTell h
+  liftIO $ hPutStrLn h placeholder
   liftIO $ hPutStrLn h "property float x"
   liftIO $ hPutStrLn h "property float y"
   liftIO $ hPutStrLn h "property float z"
   liftIO $ hPutStrLn h "end_header"
-  go h 0
+  go h 0 placeholderPos
   liftIO $ hClose h
   where
-    go h count = do
+    go h count placeholderPos = do
       may <- await
       case may of
-        Nothing -> pure () --TODO update count here "element vertex (show count)"
         Just x -> do
           liftIO $ hPutStr h (toStr x)
-          go h (count+1)
+          go h (count+1) placeholderPos
+        Nothing -> do
+          let placeholderlength = length placeholder
+              replacement = "element vertex " ++ show count ++ "\ncomment "
+          liftIO $ hSeek h AbsoluteSeek placeholderPos
+          liftIO $ hPutStrLn h $ replacement ++ replicate (placeholderlength - length replacement) '#'
+
 
     --- TODO have this helper rather in transformers? (Could already reuse a generalized version)
     toStr v = (show . getx $ v) ++ " " ++ (show . gety $ v) ++ " " ++ (show . getz $ v) ++ " \n" --TODO use "showS trick"
+
+    placeholder = "element vertex 0\ncomment ##################################"
