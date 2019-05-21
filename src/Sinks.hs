56667560cc201bfc90c4ac10f29085a84698f3dd
Martin Buck <buckmartin@buckmartin.de>
2019-05-21 20:12:57 +0200

    remove triplet sinks since the transformer can now be used

--- src/Sinks.hs
+++ src/Sinks.hs
@@ -38,9 +38,6 @@ tripletSinks :: (X a, Y a, Z a) => M.Map Format (Environment -> ConduitT (a, a, 
 tripletSinks = M.fromList
   [ (StlAscii,  stlAsciiSink)
   , (StlBinary, stlBinarySink)
-  , (PlyAscii,  plyTripletAsciiSink)
-  , (PlyBinary, plyTripletBinarySink)
-  , (Obj,       objTripletSink)
   ]
 
 faceSinks :: (X a, Y a, Z a) => M.Map Format (Environment -> ConduitT () a IO () -> ConduitT () Face IO () -> IO ())
@@ -151,55 +148,6 @@ stlBinarySink Environment{..} = do
         n = faceNormal (toPos a) (toPos b) (toPos c)
     write d  = P.putFloatle $ realToFrac d
 
---- TODO lots of duped code
-plyTripletAsciiSink :: (X a, Y a, Z a) => Environment -> ConduitT (a, a, a) Void IO ()
-plyTripletAsciiSink Environment{..} = do
-  liftIO $ hPutStr eHandle $ unlines
-    [ "ply"
-    , "format ascii 1.0"
-    ]
-  placeholderVsPos <- liftIO $ hTell eHandle
-  liftIO $ hPutStr eHandle $ unlines
-    [ placeholderVs
-    , "property float x"
-    , "property float y"
-    , "property float z"
-    ]
-  placeholderFsPos <- liftIO $ hTell eHandle
-  liftIO $ hPutStr eHandle $ unlines
-    [ placeholderFs
-    , "property list uchar int vertex_index"
-    , "end_header"
-    ]
-  go 0 placeholderVsPos placeholderFsPos
-  where
-    --- TODO super messy now, cleanup
-    go !countFs !placeholderVsPos !placeholderFsPos = do
-      may <- await
-      case may of
-        Just (a, b, c) -> do
-          liftIO $ do 
-            hPutStr eHandle (toStr a)
-            hPutStr eHandle (toStr b)
-            hPutStr eHandle (toStr c)
-          go (countFs+1) placeholderVsPos placeholderFsPos
-        Nothing -> liftIO $ do
-          let placeholderVslength = length placeholderVs
-              replacementVs       = "element vertex " ++ show (3 * countFs) ++ "\ncomment "
-              placeholderFslength = length placeholderFs
-              replacementFs       = "element face " ++ show countFs ++ "\ncomment "
-          mapM_ printFace [0..countFs-1] --- TODO error if no faces!?
-          hSeek eHandle AbsoluteSeek placeholderVsPos
-          hPutStrLn eHandle $ replacementVs ++ replicate (placeholderVslength - length replacementVs) '#'
-          hSeek eHandle AbsoluteSeek placeholderFsPos
-          hPutStrLn eHandle $ replacementFs ++ replicate (placeholderFslength - length replacementFs) '#'
-
-    --- TODO have this helper rather in transformers? (Could already reuse a generalized version)
-    toStr v       = (show . getx $ v) ++ " " ++ (show . gety $ v) ++ " " ++ (show . getz $ v) ++ " \n" --TODO use "showS trick"
-    placeholderVs = "element vertex 0\ncomment ##################################"
-    placeholderFs = "element face 0\ncomment ##################################"
-    printFace fid = hPutStrLn eHandle $ "3 " ++ (show $ 3*fid+0) ++ " " ++ (show $ 3*fid+1) ++ " " ++ (show $ 3*fid+2)
-    
 --------------------------------------------------------------------------------
 ---TODO rename
 ---TODO implement
@@ -373,61 +321,6 @@ plyOnlyFaceBinary Environment{..} = go 0
           go $ count+1
         Nothing -> pure count
     
-plyTripletBinarySink :: (X a, Y a, Z a) => Environment -> ConduitT (a, a, a) Void IO ()
-plyTripletBinarySink Environment{..} = do
-  liftIO $ hPutStr eHandle $ unlines
-    [ "ply"
-    , "format binary_big_endian 1.0"
-    ]
-  placeholderVsPos <- liftIO $ hTell eHandle
-  liftIO $ hPutStr eHandle $ unlines
-    [ placeholderVs
-    , "property float x"
-    , "property float y"
-    , "property float z"
-    ]
-  placeholderFsPos <- liftIO $ hTell eHandle
-  liftIO $ hPutStr eHandle $ unlines
-    [ placeholderFs
-    , "property list uchar int vertex_index"
-    , "end_header"
-    ]
-  go 0 placeholderVsPos placeholderFsPos
-  where
-    --- TODO super messy now, cleanup
-    go !countFs !placeholderVsPos !placeholderFsPos = do
-      may <- await
-      case may of
-        Just (a, b, c) -> do
-          liftIO $ BL.hPutStr eHandle $ P.runPut $ do 
-            writeVertex a
-            writeVertex b
-            writeVertex c
-          go (countFs+1) placeholderVsPos placeholderFsPos
-        Nothing -> liftIO $ do
-          let placeholderVslength = length placeholderVs
-              replacementVs       = "element vertex " ++ show (3 * countFs) ++ "\ncomment "
-              placeholderFslength = length placeholderFs
-              replacementFs       = "element face " ++ show countFs ++ "\ncomment "
-          mapM_ (BL.hPutStr eHandle . P.runPut . writeFace) [0..countFs-1] --- TODO error if no faces!?
-          hSeek eHandle AbsoluteSeek placeholderVsPos
-          hPutStrLn eHandle $ replacementVs ++ replicate (placeholderVslength - length replacementVs) '#'
-          hSeek eHandle AbsoluteSeek placeholderFsPos
-          hPutStrLn eHandle $ replacementFs ++ replicate (placeholderFslength - length replacementFs) '#'
-
-    --- TODO have this helper rather in transformers? (Could already reuse a generalized version)
-    placeholderVs = "element vertex 0\ncomment ##################################"
-    placeholderFs = "element face 0\ncomment ##################################"
-    writeFace fid = do
-      P.putInt8 3
-      P.putInt32be $ 3*fid+0
-      P.putInt32be $ 3*fid+1
-      P.putInt32be $ 3*fid+2
-    writeVertex v = do
-      P.putFloatbe $ realToFrac $ getx v
-      P.putFloatbe $ realToFrac $ gety v
-      P.putFloatbe $ realToFrac $ getz v
-
 --------------------------------------------------------------------------------
 
 objSink :: (X a, Y a, Z a) => Environment -> ConduitT () a IO () -> ConduitT () Face IO () -> IO ()
@@ -459,24 +352,6 @@ objOnlyFace Environment{..} = go
           go
         Nothing -> pure ()
     toStr (Face a b c) = "f " ++ show (a+1) ++ " " ++ show (b+1) ++ " " ++ show (c+1)
-
-objTripletSink :: (X a, Y a, Z a) => Environment -> ConduitT (a, a, a) Void IO ()
-objTripletSink Environment{..} = go 0
-  where
-    go !countFs = do
-      may <- await
-      case may of
-        Just (a, b, c) -> do
-          liftIO $ do 
-            hPutStr eHandle (toStr a)
-            hPutStr eHandle (toStr b)
-            hPutStr eHandle (toStr c)
-          go (countFs+1)
-        Nothing -> liftIO $ mapM_ printFace [0..countFs-1] --- TODO error if no faces!?
-
-    --- TODO have this helper rather in transformers? (Could already reuse a generalized version)
-    toStr v       = "v " ++ (show . getx $ v) ++ " " ++ (show . gety $ v) ++ " " ++ (show . getz $ v) ++ " \n" --TODO use "showS trick"
-    printFace fid = hPutStrLn eHandle $ "f " ++ (show $ 3*fid+1) ++ " " ++ (show $ 3*fid+2) ++ " " ++ (show $ 3*fid+3)
 
 xyzSink :: (X a, Y a, Z a) => Environment -> ConduitT a Void IO ()
 xyzSink Environment{..} = go
